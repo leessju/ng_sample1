3f3f4ecb9aca7687bb466751a5801240edafe9ee
leessju <leessju@gmail.com>
2018-06-17 23:37:11 +0900

    code refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
-
 import { UsersComponent } from './components/users/users.component';
-
 import { PostsComponent } from './components/posts/posts.component';
-
 import { PostComponent } from './components/post/post.component';
-
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
@@ -20,9 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  exports: [RouterModule],
-  imports: [
-    RouterModule.forRoot(routes)
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,10 @@ import { UserService } from './services/user.service';
 import { PostService } from './services/post.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { PostComponent } from './components/post/post.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-
 
 @NgModule({
   declarations: [
