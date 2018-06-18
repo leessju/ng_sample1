bc0df15b1f874981eaefb0c5960722ae69aae36e
nicejames <leessju@gmail.com>
2018-06-18 17:58:51 +0900

    add files of bulma

--- src/app/components/bulma/bulma.component.ts
+++ src/app/components/bulma/bulma.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './bulma.component.html',
-  styleUrls: ['./bulma.component.css']
+    selector: 'app-not-found',
+    templateUrl: './bulma.component.html',
+    styleUrls: ['./bulma.component.css']
 })
 export class BulmaComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
+    }
 }
